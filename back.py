fedcdae527b3c9318b73ff64fb32c44a7cc8188a
POOJA HARIHAR <88924201+poojaharihar03@users.noreply.github.com>
2024-04-26 16:45:03 +0530

    Update back.py

--- back.py
+++ back.py
@@ -48,18 +48,18 @@ def model(user_query, max_length, temp):
                                      verbose=True,
                                      chain_type_kwargs={"prompt": prompt})
     # return qa(user_query)["result"]
-    response = qa(user_query)["result"]
     answer_start = response.find("Answer:")
     if answer_start != -1:
         answer = response[answer_start + len("Answer:"):].strip()
         last_period_index = answer.rfind('.')
         if last_period_index != -1:
             answer = answer[:last_period_index + 1]
+        return answer
     else:
         assistant_index = response.find("assistant:")
         if assistant_index != -1:
             response = response[:assistant_index].strip()
-            return answer
+        return response
 
 
         
